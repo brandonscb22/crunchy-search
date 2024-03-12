8505811955a275e9cb8c21b97d2b82811302a6f6
Brandon Lopez <brandonscb95@gmail.com>
2024-03-12 16:40:59 -0500

    First example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MatGridListModule } from '@angular/material/grid-list';
@@ -9,43 +9,93 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
 import { AppService } from './app.service';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatGridListModule, MatCardModule, MatSelectModule, MatInputModule, MatFormFieldModule, MatButtonModule, HttpClientModule],
+  imports: [CommonModule, RouterOutlet, MatGridListModule, MatCardModule, MatSelectModule, MatInputModule, MatFormFieldModule, MatButtonModule, HttpClientModule, MatTableModule, ReactiveFormsModule, MatPaginatorModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
 })
+
 export class AppComponent implements OnInit {
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  searchForm = new FormGroup({
+    title: new FormControl(''),
+    audio: new FormControl(''),
+    ratings: new FormControl(''),
+  });
+  displayedColumns: string[] = ['poster', 'title', 'maturity_ratings', 'audio_locales', 'episode_count'];
+  columnsToDisplay: string[] = this.displayedColumns.slice();
   token: any;
   totalSeries: number = 0;
   totalPaginates: number = 0;
-  public series: { title: string }[] = [{ title: 'R' }];
+  data: serieElement[] = [{ title: 'R' }];
+  dataSource = new MatTableDataSource<serieElement>(this.data);
   title = 'crunchy-search';
+
   constructor(private appService: AppService) {
     this.appService.getToken().subscribe(response => {
       this.token = (response as { access_token: string; }).access_token;
-      console.log(this.token);
       this.appService.getPaginates(0, this.token).subscribe(response => {
-        this.series = (response as { data: { title: string }[], total: number }).data;
+        this.data = (response as {
+          data: { title: string }[], total: number
+        }).data;
         this.totalSeries = (response as { data: object[], total: number }).total;
         this.totalPaginates = Math.ceil(this.totalSeries / 50);
-        console.log(this.series.length);
         for (let i = 1; i <= this.totalPaginates; i++) {
-          console.log(i * 50);
           this.appService.getPaginates(i * 50, this.token).subscribe(rsp => {
             let data = (rsp as { data: { title: string }[] }).data;
-            this.series = [...this.series, ...data];
-            console.log('series', this.series.length);
-            console.log('serie0', this.series[1]['title'])
+            this.data = [...this.data, ...data];
+            this.dataSource.data = this.data;
           })
         }
       })
     });
   }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
   async ngOnInit() {
 
   }
+
+  async onSubmit() {
+    console.warn(this.searchForm.value.title);
+    console.warn(this.searchForm.value.ratings);
+    console.warn(this.searchForm.value.audio);
+    console.log('Se logro')
+    const dataSearch = this.data.filter(objeto => {
+      let cumpleCriterio = true;
+
+      if (this.searchForm.value.title) {
+        cumpleCriterio = objeto.title.toLowerCase().includes(this.searchForm.value.title.toLowerCase() ?? '') ?? false
+      }
+      // Aplicar filtro por parte de texto si la condición se cumple
+      if (this.searchForm.value.ratings && cumpleCriterio) {
+        cumpleCriterio = objeto.series_metadata?.maturity_ratings?.includes(this.searchForm.value.ratings ?? '') ?? false
+      }
+
+      // Aplicar filtro por categoría si la condición se cumple
+      if (this.searchForm.value.audio && cumpleCriterio) {
+        cumpleCriterio = objeto.series_metadata?.audio_locales?.some(subElemento => subElemento.includes(this.searchForm.value.audio ?? '')) ?? false
+      }
+
+      return cumpleCriterio;
+
+    })
+    this.dataSource.data = dataSearch;
+    this.dataSource.paginator ? this.dataSource.paginator.firstPage() : ''
+  }
+}
+export interface serieElement {
+  title: string;
+  images?: { poster_wide?: { source?: string }[][] };
+  series_metadata?: { maturity_ratings?: string[], audio_locales?: string[], episode_count: number }
 }
